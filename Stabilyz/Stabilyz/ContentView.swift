690f0366e9490e9c1c5b21aa3b136b4258fd1f5d
Adi <61893663+yosephadi@users.noreply.github.com>
2026-09-09 03:53:02 +0800

    Add Xcode project skeleton

---
+++ Stabilyz/Stabilyz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stabilyz
+//
+//  Created by Adi's Mac on 09/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stabilyz/Stabilyz/StabilyzApp.swift
@@ -0,0 +1,17 @@
+//
+//  StabilyzApp.swift
+//  Stabilyz
+//
+//  Created by Adi's Mac on 09/09/26.
+//
+
+import SwiftUI
+
+@main
+struct StabilyzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
