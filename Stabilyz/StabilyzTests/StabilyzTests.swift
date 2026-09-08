86af76cd009ec164be6b631331052df97dc9ddf6
Adi <61893663+yosephadi@users.noreply.github.com>
2026-09-09 04:44:43 +0800

    Add test targets

---
+++ Stabilyz/StabilyzTests/StabilyzTests.swift
@@ -0,0 +1,18 @@
+//
+//  StabilyzTests.swift
+//  StabilyzTests
+//
+//  Created by Adi's Mac on 09/09/26.
+//
+
+import Testing
+
+struct StabilyzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
